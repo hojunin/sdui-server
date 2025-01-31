25cc74a16140c2935dd54da26bc26c275be4ac21
hojunin <dlsghwns@naver.com>
2025-01-31 19:11:58 +0900

    feat: cors 룰 추가

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS 설정 추가
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   await app.listen(3001);
 }
 bootstrap();
